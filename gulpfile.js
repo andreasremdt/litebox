9a830cb47f8af494dfbf6038e4c779bb98d636da
Andreas Remdt <andreas.remdt@gmail.com>
2018-09-16 13:17:51 +0200

    add new babel pipeline, remove old gulp code

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,4 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const jslint = require('gulp-jslint');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
@@ -14,17 +11,9 @@ gulp.task('scss', () => {
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('js', () => {
-  return gulp.src('src/js/litebox.js')
-    .pipe(babel())
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/js'));
-});
 
 gulp.task('watch', () => {
   gulp.watch('src/scss/*.scss', ['scss']);
-  gulp.watch('src/js/*.js', ['js']);
 });
 
-gulp.task('default', ['js', 'scss', 'watch']);
+gulp.task('default', ['scss', 'watch']);
